2c06932ee58e381ed7ecfd411d0fb536c34e37ac
hunterkepley <kepley.l.hunter@gmail.com>
2021-08-14 01:47:34 -0400

    Removed camera zoom

--- camera.go
+++ camera.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/hajimehoshi/ebiten"
-)
-
 type Camera struct {
 	position    Vec2f
 	cameraSpeed float64
@@ -21,11 +17,5 @@ func createCamera(position Vec2f, cameraSpeed float64) Camera {
 }
 
 func (c *Camera) update() {
-	_, wY := ebiten.Wheel()
-	if wY > 0 && c.zoom < 1.4 {
-		c.zoom += 0.05
-	} else if wY < 0 && c.zoom > .4 {
-		c.zoom = ((c.zoom * 100) - 5) / 100
-	}
 	c.size = Vec2f{screenWidth + (screenWidth - screenWidth*c.zoom), screenHeight + (screenHeight - screenHeight*c.zoom)}
 }
